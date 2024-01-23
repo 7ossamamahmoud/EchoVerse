b7a4cfd4747d6b787dc17d4947c6f6471a396c4c
7ossaamamahmoud <ossamamahmoud845@gmail.com>
2024-01-23 18:45:50 +0200

    Finishing Splash Screen without adding Animation.

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,10 +1,28 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(
+          "assets/images/Logo.png",
+        ),
+        Text(
+          "Read, discover, escape: limitless worlds await.",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 12.5.dp,
+            color: Colors.white70,
+            fontFamily: "GT",
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:echo_verse/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:get/get.dart';
+
+import 'constants.dart';
 
 void main() {
   runApp(
@@ -13,9 +16,16 @@ class EchoVerse extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashView(),
+    return FlutterSizer(
+      builder: (BuildContext, Orientation, ScreenType) {
+        return GetMaterialApp(
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+          ),
+          debugShowCheckedModeBanner: false,
+          home: const SplashView(),
+        );
+      },
     );
   }
 }
