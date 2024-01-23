9120f4493f88a5b719e0db741832fd5d09dd4350
7ossaamamahmoud <ossamamahmoud845@gmail.com>
2024-01-23 23:07:25 +0200

    Adding Fade Transition.

--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,28 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset(
-          "assets/images/Logo.png",
-        ),
-        Text(
-          "Read, discover, escape: limitless worlds await.",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 12.5.dp,
-            color: Colors.white70,
-            fontFamily: "GT",
-          ),
-        )
-      ],
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+        milliseconds: 500,
+      ),
     );
+    fadeAnimation =
+        Tween<double>(begin: 0, end: 1).animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: fadeAnimation,
+        builder: (context, _) {
+          return FadeTransition(
+            opacity: fadeAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.asset(
+                  "assets/images/Logo.png",
+                ),
+                Text(
+                  "Read, discover, escape: limitless worlds await.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 12.5.dp,
+                    color: Colors.white70,
+                    fontFamily: "GT",
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 }
